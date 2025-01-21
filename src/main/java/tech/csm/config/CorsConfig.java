2ae8f229db04dd03b1818dd35e747d21af979473
Somen Mitra <somenmitra22082000@gmail.com>
2025-01-21 14:47:26 +0530

    some cors changes

--- src/main/java/tech/csm/config/CorsConfig.java
+++ src/main/java/tech/csm/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200")); // Frontend origin
+        config.setAllowedOrigins(List.of("http://localhost:4200", "https://estoreapl.netlify.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
